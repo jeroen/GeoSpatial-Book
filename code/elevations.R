5549fd688ac2643909b92bc02e96929c008727ef
Xiangyun Huang <xiangyunfaith@outlook.com>
2018-10-31 20:05:45 +0800

    以气泡图和散点图的方式绘制 elevation 数据

--- code/elevations.R
+++ code/elevations.R
@@ -3,24 +3,29 @@ dat <- read.table(file = "data/NOTREDAM.TXT", header = T)
 
 with(dat, {
   plot(c(0,7),c(0,7),type = "n", xlab = "East-West", ylab = "North-South")
-  points(North.South ~ East.West, pch = 16, col = )
-  text(
-    x = East.West, y = North.South,
-    labels = Elevation, adj = c(0, 0), xpd = 1
-  )
+  points(North.South ~ East.West, pch = 21, 
+         bg = gray(1,alpha = 0.5), cex = (Elevation-600)/90)
 })
 
-# 气泡图 [@Tanimura2006]
- i = 1:4
- 600 + 90*i
-# 左开右闭区间 
-0-1
-1-2
-2-3
-3-4
-(dat$Elevation - 600)/90
+cols =  gray.colors(4) # terrain.colors(4) 
+elev2col <- function(x) {
+  if (x <= 1) {
+    col <- cols[1]
+  } else if (x <= 2) {
+    col <- cols[2]
+  } else if (x <= 3) {
+    col <- cols[3]
+  } else if (x <= 4)
+    col <- cols[4]
+}
 
-?gray()
+dat <- transform(dat, elev2col = sapply((Elevation - 600)/90, elev2col, USE.NAMES = TRUE) )
 
+with(dat, {
+  plot(c(0,7),c(0,7),type = "n", xlab = "East-West", ylab = "North-South")
+  for (i in seq(dim(dat)[1])) {
+    points(dat[i,c(1,2)], pch = 21, bg = dat[i,4], cex =2 ) 
+  }
+})
 
 
